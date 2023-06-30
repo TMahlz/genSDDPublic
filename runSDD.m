137603c944909ad2d71bfdc0e17d1723c2485604
tmahlman <tmahlman@mathworks.com>
2023-06-30 10:43:02 -0700

    Added some debugging print statements

--- runSDD.m
+++ runSDD.m
@@ -1,5 +1,11 @@
+disp("Beginning runSDD execution...")
+
 % Extract Project
+disp("Opening Project...")
 project = openProject(pwd);
+disp("Project Opened successfully...")
 
 % Run 'genSDD' on 'testModel'
+disp("Executing genSDD...")
 genSDD('TestModel',[],'CI');
+disp("genSDD Ran Successfully...")
